b578e6fdf5eb0fa759509f42004f98c43bb7403f
ayushjrathod <clashofa1057@gmail.com>
2025-04-16 19:50:26 +0530

    testt

--- main.py
+++ main.py
@@ -20,10 +20,13 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://outofbounds.vercel.app"
+    ],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
+    expose_headers=["*"]
 )
 
 class ChatInput(BaseModel):
